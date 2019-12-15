955d3b098f922f64a4493e01aefbf16bb67ffd1c
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2019-12-15 13:38:43 +0100

    Initial commit

---
+++ src/test/java/com/TMT/TmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
