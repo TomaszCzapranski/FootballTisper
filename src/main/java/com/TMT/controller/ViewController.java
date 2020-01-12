3efa0e46458035770e305fcae8ff7d470da4cc6e
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-01-12 14:30:25 +0100

    Registration already works.

--- src/main/java/com/TMT/controller/ViewController.java
+++ src/main/java/com/TMT/controller/ViewController.java
@@ -1,21 +1,32 @@
 package com.TMT.controller;
 
+import com.TMT.model.users.Profile;
 import com.TMT.service.FixtureRepository;
+import com.TMT.service.ProfileManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
 public class ViewController {
 
     private FixtureRepository fixtureRepository;
+    private ProfileManager profileManager;
 
     @Autowired
-    public ViewController(FixtureRepository fixtureRepository) {
+    public ViewController(FixtureRepository fixtureRepository, ProfileManager profileManager) {
         this.fixtureRepository = fixtureRepository;
+        this.profileManager = profileManager;
     }
 
     @GetMapping
@@ -30,5 +41,9 @@ public class ViewController {
         return "register";
     }
 
-
+    @PostMapping
+    public RedirectView addCat(Profile profile) {
+        profileManager.addProfile(profile);
+        return new RedirectView("/");
+    }
 }
