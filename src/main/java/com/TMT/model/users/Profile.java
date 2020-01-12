6ceb005ea314c309b2c3e7321fc8ae231aa8871a
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-01-12 10:23:10 +0100

    Set of fixtures added to user profile

--- src/main/java/com/TMT/model/users/Profile.java
+++ src/main/java/com/TMT/model/users/Profile.java
@@ -1,8 +1,12 @@
 package com.TMT.model.users;
 
+import com.TMT.model.matches.Fixture;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.List;
+import java.util.Set;
 
 @Entity(name = "profile")
 public class Profile {
@@ -19,5 +23,7 @@ public class Profile {
 
     private Long points;
 
+    private Set<Fixture> placedBets;
 
+    private List<Fixture> userBets;
 }
